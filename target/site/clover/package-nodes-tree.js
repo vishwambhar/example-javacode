cb83340af89c487e7ce835601505fa4b761782b6
vishyoo <vishyoo@yahoo-inc.com>
2016-06-23 12:05:48 -0700

    initial checkin

---
+++ target/site/clover/package-nodes-tree.js
@@ -0,0 +1,47 @@
+ 
+
+var Packages = {
+    nodes: [
+                                                                                                        
+                
+{
+    "id": "com.example.sample.rest",
+    "text": "com.example.sample.rest",
+    "package": "com.example.sample.rest",
+    "url": "com/example/sample/rest/pkg-summary.html",
+            "coverage": "100%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "com/example/sample/rest/pkg-summary.html"},
+    "children": [
+                                            
+                
+{
+    "id": "com.example.sample.rest.services",
+    "text": "services",
+    "package": "com.example.sample.rest.services",
+    "url": "com/example/sample/rest/services/pkg-summary.html",
+            "coverage": "100%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "com/example/sample/rest/services/pkg-summary.html"},
+    "children": [
+                    ]
+},
+            ]
+},
+            ],
+    settings: {
+        "icons": {
+            "package": {
+                "open": "aui-icon aui-icon-small aui-iconfont-devtools-folder-open",
+                "closed": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed"
+            },
+            "state": {
+                "collapsed": "aui-icon aui-icon-small aui-iconfont-collapsed",
+                "expanded": "aui-icon aui-icon-small aui-iconfont-expanded",
+                "forRemoval": "hidden aui-iconfont-collapsed aui-iconfont-expanded"
+            }
+        }
+    }
+};
---
+++ target/site/clover/treemap-dash-json.js
@@ -0,0 +1,12 @@
+var treeMapJson = {"id":"Clover database Thu Jun 23 2016 12:04:30 PDT0","name":"","data":{
+    "$area":53.0,"$color":100.0,"title":" 53 Elements, 100% Coverage"},
+  "children":[{"id":"com.example.sample.rest0","name":
+      "com.example.sample.rest","data":{"$area":44.0,"$color":100.0,"title":
+        "com.example.sample.rest 44 Elements, 100% Coverage"},"children":[]},
+    {"id":"com.example.sample.rest.services44","name":
+      "com.example.sample.rest.services","data":{"$area":9.0,"$color":
+        100.0,"title":
+        "com.example.sample.rest.services 9 Elements, 100% Coverage"},
+      "children":[]}]}
+;
+processTreeMapDashJson (treeMapJson);
